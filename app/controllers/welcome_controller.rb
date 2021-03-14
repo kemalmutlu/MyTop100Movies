fbe0eefa5a8fd9a070ec3fe985c2c12b28d8a8ba
sezeristif <sezeristif@gmail.com>
2021-03-14 17:48:51 +0300

    [add] httparty for http requests

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @movies = HTTParty.get("https://api.themoviedb.org/3/trending/movie/day?api_key=#{ENV['TMDB_API_KEY']}")['results']
   end
 end
