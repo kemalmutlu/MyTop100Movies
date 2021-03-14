8415074a681e6dfe980752df1e431df0446b5761
Kemal Mutlu <mutlukemal1717@gmail.com>
2021-03-14 18:19:42 +0300

    [update] tmdb api cache in welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,15 @@
 class WelcomeController < ApplicationController
+  before_action :get_movies, only: :index
+
   def index
-    @movies = HTTParty.get("https://api.themoviedb.org/3/trending/movie/day?api_key=#{ENV['TMDB_API_KEY']}")['results']
+    @movies = Rails.cache.read("movies")
+  end
+
+  private
+  def get_movies
+    unless Rails.cache.read("movies")
+      @movies = HTTParty.get("https://api.themoviedb.org/3/trending/movie/day?api_key=#{ENV['TMDB_API_KEY']}")['results']
+      Rails.cache.write("movies", @movies, :expires_in => 30.minutes)
+    end
   end
 end
